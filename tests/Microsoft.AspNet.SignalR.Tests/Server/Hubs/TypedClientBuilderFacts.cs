3640f0bf51da101ceb80a30664191c152f933c4b
Stephen Halter <halter73@gmail.com>
2013-08-08 20:37:10 -0700

    WIP

---
+++ tests/Microsoft.AspNet.SignalR.Tests/Server/Hubs/TypedClientBuilderFacts.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Tests.Server.Hubs
+{
+    public class TypedClientBuilderFacts
+    {
+        [Fact]
+        public void MethodsAreInvokedThrougIClientProxy()
+        {
+            var mockClientProxy = new Mock<IClientProxy>(MockBehavior.Strict);
+            mockClientProxy.Setup(c => c.Invoke(
+                    It.Is<string>(methodName => methodName == "send"),
+                    It.Is<string>(argument => argument == "fun!")))
+                .Returns(Task.FromResult<object>(null));
+
+            var client = TypedClientBuilder<IClientContract>.Build(mockClientProxy.Object);
+
+            client.send("fun!");
+
+            mockClientProxy.Verify();
+        }
+
+        public interface IClientContract
+        {
+            void send(string messages);
+        }
+    }
+}
